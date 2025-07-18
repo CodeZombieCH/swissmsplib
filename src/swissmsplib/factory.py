a6802955a3398fb8d4f887d2c21f15d4e81aeb98
Marc-André Bühler <git@codezombie.ch>
2025-07-18 23:51:11 +0000

    Adjusted clients to changes to underlying web apps
     - salt: Introduced new login URL
     - sunrise: Switched to new URL
     - Improved unit test coverage

--- src/swissmsplib/factory.py
+++ src/swissmsplib/factory.py
@@ -5,17 +5,25 @@ from swissmsplib.swisscom import SwisscomClient
 
 def create_client(provider) -> SaltClient | SwisscomClient | SunriseClient:
     if provider == "salt":
-        return SaltClient("https://my.salt.ch/", "myaccount-ui-service")
+        return SaltClient(
+            login_url="https://login.salt.ch",
+            service_url="https://my.salt.ch",
+            service_name="myaccount-ui-service",
+        )
     elif provider == "gomo":
-        return SaltClient("https://my-gomo.salt.ch/", "myaccount-gomo-ui-service")
+        return SaltClient(
+            login_url="https://login.go-mo.ch",
+            service_url="https://my.go-mo.ch",
+            service_name="myaccount-gomo-ui-service",
+        )
     elif provider == "mbudget":
         return SwisscomClient("https://selfcare.m-budget.migros.ch")
     elif provider == "coopmobile":
         return SwisscomClient("https://myaccount.coopmobile.ch")
     elif provider == "yallo":
-        return SunriseClient("https://ms.yallo.ch", "yallo")
+        return SunriseClient("https://prod.ms.api.yallo.ch", "yallo")
     elif provider == "lebara":
-        return SunriseClient("https://ms.lebara.ch", "lebara")
+        return SunriseClient("https://prod.ms.api.lebara.ch", "lebara")
 
     else:
         raise Exception(f"provider {provider} is not supported")
--- tests/test_gomo.py
+++ tests/test_gomo.py
@@ -3,10 +3,10 @@ import os
 
 from swissmsplib.factory import create_client
 from swissmsplib.profiles import read_profile
-from swissmsplib.swisscom import SwisscomClient
+from swissmsplib.salt import SaltClient
 
 
-class TestMBudgetClient(unittest.TestCase):
+class TestGoMoClient(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.__client = cls.__get_client()
@@ -16,11 +16,11 @@ class TestMBudgetClient(unittest.TestCase):
         cls.__client.logout()
 
     @staticmethod
-    def __get_client() -> SwisscomClient:
+    def __get_client() -> SaltClient:
         os.environ["PROFILE_FILE"] = "./.data/profiles.toml"
-        profile = read_profile("mbudget")
+        profile = read_profile("gomo")
 
-        client: SwisscomClient = create_client("mbudget")  # type: ignore
+        client: SaltClient = create_client("gomo")  # type: ignore
         client.login(profile.username, profile.password)
         return client
 
@@ -29,9 +29,3 @@ class TestMBudgetClient(unittest.TestCase):
 
         subscriptions = client.get_subscriptions()
         self.assertGreater(len(subscriptions), 0)
-
-    def test_get_prepaid_balance(self):
-        client = self.__client
-
-        balance = client.get_prepaid_balance()
-        self.assertGreaterEqual(balance, 0)
